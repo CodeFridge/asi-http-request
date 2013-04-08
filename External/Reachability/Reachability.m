8ac7d1fd6e0e114511dde6ccb0cb4abc9f7cbf46
Sean Beausoleil <sean@orchestra.com>
2013-04-08 12:42:29 -0700

    cleanup some unused variable references

--- External/Reachability/Reachability.m
+++ External/Reachability/Reachability.m
@@ -161,13 +161,13 @@ static void logNetworkStatus_(const char *name, int line, NetworkStatus status) 
 	
 	switch (status) {
 		case kNotReachable:
-			statusString = @"Not Reachable";
+			//statusString = @"Not Reachable";
 			break;
 		case kReachableViaWWAN:
-			statusString = @"Reachable via WWAN";
+			//statusString = @"Reachable via WWAN";
 			break;
 		case kReachableViaWiFi:
-			statusString = @"Reachable via WiFi";
+			//statusString = @"Reachable via WiFi";
 			break;
 	}
 	
